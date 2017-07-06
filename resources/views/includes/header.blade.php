4eba8db1f918e0a1ca9c7ab155c33567186b6cc9
Diego <diegojsguerrero@gmail.com>
2017-07-06 13:39:27 -0400

    My first commit into Heroku

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="{{ route('dashboard') }}">Cara de Pollo</a>
+      <a class="navbar-brand" href="{{ route('dashboard') }}">Social Net</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
